30ed563c7853cd5092be6610c8e6411fd50e7d81
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2023-03-24 14:35:48 +0800

    baru

---
+++ resources/views/konten.blade.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--begin::Head-->
+	<head><base href="../../">
+		<title>RSDARA</title>
+		<meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
+		<meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8" />
+		<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+		<link rel="shortcut icon" href="{{ asset('src/media/logos/favicon.ico') }}" />		
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+		<link href="{{ asset('src/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('src/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+		<link href="{{ asset('src/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+		
+	</head>
+	<!--end::Head-->
+	<!--begin::Body-->
+	<body id="kt_body" data-kt-aside-minimize="on" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed toolbar-tablet-and-mobile-fixed aside-enabled aside-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+		<!--begin::Main-->
+		<!--begin::Root-->
+		<div class="d-flex flex-column flex-root">
+			<!--begin::Page-->
+			<div class="page d-flex flex-row flex-column-fluid">
+				<!--begin::Aside-->
+				<div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
+					<!--begin::Brand-->
+					<div class="aside-logo flex-column-auto" id="kt_aside_logo">
+						<!--begin::Logo-->
+						<a href="../../demo13/dist/index.html">
+							<img alt="Logo" src="src/media/logos/logo-demo13.svg" class="h-15px logo" />
+						</a>
+						<!--end::Logo-->
+						<!--begin::Aside toggler-->
+						<div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
+							<!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
+							<span class="svg-icon svg-icon-1 rotate-180">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</div>
+						<!--end::Aside toggler-->
+					</div>
+					<!--end::Brand-->
+					<!--begin::Aside menu-->
+					<div class="aside-menu flex-column-fluid">
+						<!--begin::Aside Menu-->
+						<div class="hover-scroll-overlay-y my-2 py-5 py-lg-8" id="kt_aside_menu_wrapper" data-kt-scroll="true" data-kt-scroll-activate="{default: false, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_aside_logo, #kt_aside_footer" data-kt-scroll-wrappers="#kt_aside_menu" data-kt-scroll-offset="0">
+							<!--begin::Menu-->
+							<div class="menu menu-column menu-title-gray-800 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500" id="#kt_aside_menu" data-kt-menu="true">
+								<div class="menu-item">
+									<div class="menu-content pb-2">
+										<span class="menu-section text-muted text-uppercase fs-8 ls-1">Dashboard</span>
+									</div>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link {{ Request::is('DashboardCariPasien') ? 'active' : '' }}" href="/DashboardCariPasien">
+										<span class="menu-icon ">
+											<i class="bi bi-grid fs-3"></i>
+										</span>
+										<span class="menu-title">Dashboard Pasien</span>
+									</a>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link {{ Request::is('CobaTampilan') ? 'active' : '' }}" href="/CobaTampilan">
+										<span class="menu-icon ">
+											<i class="bi bi-grid fs-3"></i>
+										</span>
+										<span class="menu-title">Coba Tampilkan</span>
+									</a>
+								</div>
+							</div>
+							<!--end::Menu-->
+						</div>
+					</div>
+					<!--end::Aside menu-->
+					<!--begin::Footer-->
+					<div class="aside-footer flex-column-auto pt-5 pb-7 px-5" id="kt_aside_footer">
+						<a href="../../demo13/dist/documentation/getting-started.html" class="btn btn-custom btn-primary w-100" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss-="click" title="200+ in-house components and 3rd-party plugins">
+							<span class="btn-label">Docs &amp; Components</span>
+							<!--begin::Svg Icon | path: icons/duotune/general/gen005.svg-->
+							<span class="svg-icon btn-icon svg-icon-2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+									<path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM15 17C15 16.4 14.6 16 14 16H8C7.4 16 7 16.4 7 17C7 17.6 7.4 18 8 18H14C14.6 18 15 17.6 15 17ZM17 12C17 11.4 16.6 11 16 11H8C7.4 11 7 11.4 7 12C7 12.6 7.4 13 8 13H16C16.6 13 17 12.6 17 12ZM17 7C17 6.4 16.6 6 16 6H8C7.4 6 7 6.4 7 7C7 7.6 7.4 8 8 8H16C16.6 8 17 7.6 17 7Z" fill="black" />
+									<path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="black" />
+								</svg>
+							</span>
+							<!--end::Svg Icon-->
+						</a>
+					</div>
+					<!--end::Footer-->
+				</div>
+				<!--end::Aside-->
+				<!--begin::Wrapper-->
+				<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+					<!--begin::Header-->
+					<div id="kt_header" style="" class="header align-items-stretch">
+						<!--begin::Container-->
+						<div class="container-fluid d-flex align-items-stretch justify-content-between">
+							<!--begin::Aside mobile toggle-->
+							<div class="d-flex align-items-center d-lg-none ms-n3 me-1" title="Show aside menu">
+								<div class="btn btn-icon btn-active-color-white" id="kt_aside_mobile_toggle">
+									<i class="bi bi-list fs-1"></i>
+								</div>
+							</div>
+							<!--end::Aside mobile toggle-->
+							<!--begin::Mobile logo-->
+							<div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
+								<a href="../../demo13/dist/index.html" class="d-lg-none">
+									<img alt="Logo" src="src/media/logos/logo-demo13-compact.svg" class="h-25px" />
+								</a>
+							</div>
+							<!--end::Mobile logo-->
+							<!--begin::Wrapper-->
+							<div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1">
+								<!--begin::Navbar-->
+								<div class="d-flex align-items-stretch" id="kt_header_nav">
+									<!--begin::Menu wrapper-->
+									<div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
+										<!--begin::Menu-->
+										<div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
+											<div class="menu-item me-lg-1">
+												<a class="menu-link py-3" href="/">
+													<span class="menu-title">SISTEM INFORMASI IBU HAMIL</span>
+												</a>
+											</div>
+										</div>
+										<!--end::Menu-->
+									</div>
+									<!--end::Menu wrapper-->
+								</div>
+								<!--end::Navbar-->
+								<!--begin::Topbar-->
+								<div class="d-flex align-items-stretch flex-shrink-0">
+									<!--begin::Toolbar wrapper-->
+									<div class="topbar d-flex align-items-stretch flex-shrink-0">
+										<!--begin::User-->
+										<div class="d-flex align-items-stretch" id="kt_header_user_menu_toggle">
+											<!--begin::Menu wrapper-->
+											<div class="topbar-item cursor-pointer symbol px-3 px-lg-5 me-n3 me-lg-n5 symbol-30px symbol-md-35px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" data-kt-menu-flip="bottom">
+												<img src="src/media/avatars/150-2.jpg" alt="metronic" />
+											</div>
+											<!--begin::Menu-->
+											<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+												<!--begin::Menu item-->
+												<div class="menu-item px-3">
+													<div class="menu-content d-flex align-items-center px-3">
+														<!--begin::Avatar-->
+														<div class="symbol symbol-50px me-5">
+															<img alt="Logo" src="src/media/avatars/150-26.jpg" />
+														</div>
+														<!--end::Avatar-->
+														<!--begin::Username-->
+														<div class="d-flex flex-column">
+															<div class="fw-bolder d-flex align-items-center fs-5">Max Smith
+															<span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">Pro</span></div>
+															<a href="#" class="fw-bold text-muted text-hover-primary fs-7">max@kt.com</a>
+														</div>
+														<!--end::Username-->
+													</div>
+												</div>
+												<!--end::Menu item-->
+												<!--begin::Menu separator-->
+												<div class="separator my-2"></div>
+												<!--end::Menu separator-->
+												<!--begin::Menu item-->
+												
+												<!--begin::Menu item-->
+												<div class="menu-item px-5">
+													<a href="/login" class="menu-link px-5">Sign Out</a>
+												</div>
+												<!--end::Menu item-->
+												<!--begin::Menu separator-->
+												<div class="separator my-2"></div>
+												<!--end::Menu separator-->
+												<!--begin::Menu item-->
+												<!--end::Menu item-->
+											</div>
+											<!--end::Menu-->
+											<!--end::Menu wrapper-->
+										</div>
+										<!--end::User -->
+										<!--begin::Heaeder menu toggle-->
+										<div class="d-flex align-items-stretch d-lg-none px-3 me-n3" title="Show header menu">
+											<div class="topbar-item" id="kt_header_menu_mobile_toggle">
+												<i class="bi bi-text-left fs-1"></i>
+											</div>
+										</div>
+										<!--end::Heaeder menu toggle-->
+									</div>
+									<!--end::Toolbar wrapper-->
+								</div>
+								<!--end::Topbar-->
+							</div>
+							<!--end::Wrapper-->
+						</div>
+						<!--end::Container-->
+					</div>
+					<!--end::Header-->
+					<!--begin::Content-->
+					
+						
+						<!--begin::Post-->
+						@yield('KONTEN')
+						<!--end::Post-->
+					
+					<!--end::Content-->
+				</div>
+				<!--end::Wrapper-->
+			</div>
+			<!--end::Page-->
+		</div>
+		<!--end::Root-->
+		<script>var hostUrl = "/";</script>
+		<script src="{{ asset('src/plugins/global/plugins.bundle.js') }}"></script>
+		<script src="{{ asset('src/js/scripts.bundle.js') }}"></script>
+		<script src="{{ asset('src/js/custom/widgets.js') }}"></script>
+		<script src="{{ asset('src/js/custom/apps/chat/chat.js') }}"></script>
+		<script src="{{ asset('src/js/custom/modals/create-app.js') }}"></script>
+		<script src="{{ asset('src/js/custom/modals/upgrade-plan.js') }}"></script>
+		<script src="{{ asset('src/js/custom/pages/projects/settings/settings.js') }}"></script>
+		<script src="{{ asset('src/js/custom/modals/users-search.js') }}"></script>
+		<script src="{{ asset('src/js/custom/modals/new-target.js') }}"></script>
+	</body>
+	<!--end::Body-->
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/login', function () {
+    return view('Login.login');
+});
+
+Route::get('/', function () {
+    return view('Konten.Dashboard');
+});
+
+Route::get('/DashboardCariPasien', function () {
+    return view('Konten.DashboardCariPasien');
+});
+Route::get('/CobaTampilan', function () {
+    return view('Konten.CobaTampil');
+});
